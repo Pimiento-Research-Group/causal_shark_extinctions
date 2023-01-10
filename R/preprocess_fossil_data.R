91bb377a59a9f792d988809cce6ecbc8bd1f7616
Ischi94 <gmathes94@gmx.de>
2023-01-10 13:27:53 +0100

    calculate extinction signal

--- R/preprocess_fossil_data.R
+++ R/preprocess_fossil_data.R
@@ -6,6 +6,17 @@ library(here)
 # plotting configurations
 source(here("R", "config_file.R"))
 
+# stage data --------------------------------------------------------------
+
+# load 95 bin Phanerozoic time scale based on the stratigraphic stages 
+# of Gradstein et al. 2020
+load(here("data", 
+          "stages.Rdata"))
+
+# clean up for binning
+dat_stages <- stages %>% 
+  as_tibble() %>% 
+  select(stg, bottom, top)
 
 # load species data ---------------------------------------------------------------
 
@@ -23,4 +34,30 @@ dat_species <- read_delim(here("data",
                values_to = "extinction_age") %>% 
   group_by(species) %>% 
   summarise(ori_age = mean(origination_age),
-            ext_age = mean(extinction_age))
+            ext_age = mean(extinction_age)) %>% 
+  # bin fad and lad to stages
+  mutate(bin_ori = 95 - cut(ori_age, breaks = dat_stages$bottom,
+                            include.lowest = TRUE,
+                            labels = FALSE), 
+         bin_ext = 95 - cut(ext_age, breaks = dat_stages$bottom,
+                            include.lowest = TRUE,
+                            labels = FALSE)) %>% 
+  drop_na(bin_ori, bin_ext) %>% 
+  # fill in duration bins
+  mutate(bin_occ = map2(.x = bin_ori, 
+                        .y = bin_ext,
+                        .f = ~ seq(.x, .y, by = 1))) %>% 
+  select(species, bin_occ, bin_ext) %>% 
+  unnest(bin_occ) %>% 
+  # create extinction signal
+  group_by(species) %>% 
+  mutate(ext_signal = if_else(bin_occ == bin_ext, 1, 0)) %>% 
+  # clean up
+  ungroup() %>% 
+  select(-bin_ext, 
+         bin = bin_occ)
+
+
+list.files(here("data"), pattern = "*full*")
+
+divDyn:::stag
