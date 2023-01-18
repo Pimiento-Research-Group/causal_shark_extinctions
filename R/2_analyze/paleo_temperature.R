543588178ca8ada2080ec974d53a6a9c2ec14677
Ischi94 <gmathes94@gmx.de>
2023-01-18 15:26:26 +0100

    add paleotemp inset

--- R/2_analyze/paleo_temperature.R
+++ R/2_analyze/paleo_temperature.R
@@ -4,6 +4,7 @@ library(here)
 library(dagitty)
 library(brms)
 library(tidybayes)
+library(patchwork)
 
 # plotting configurations
 source(here("R", "config_file.R"))
@@ -154,9 +155,73 @@ plot_paleotemp <- dat_pred %>%
        x = "Temperature Change [°C]") +
   theme(legend.position = "none")
 
+
+
+# estimate trend ----------------------------------------------------------
+
+# average posterior draws by model stacking
+dat_pred_post <- posterior_average(mod1, mod2,
+                                   mod3, mod4,
+                                   mod5, mod6,
+                                   mod7, mod8,
+                                   seed = 1708,
+                                   ndraws =  1e4, 
+                                   missing = NA) %>% 
+  select(contains("temp")) %>% 
+  pivot_longer(cols = everything(), 
+               names_to = "coef_name", 
+               values_to = "coef_val") %>% 
+  drop_na(coef_val)
+
+
+
+# visualise
+plot_paleotemp_beta <- dat_pred_post %>%
+  ggplot(aes(coef_val, fill = coef_name)) +
+  geom_vline(xintercept = 0, 
+             linewidth = 0.5,
+             colour = "grey40") +
+  stat_slab(shape = 21, 
+            slab_colour = NA, 
+            slab_alpha = 0.7,
+            point_size = 3, 
+            point_fill = "white",
+            point_colour = colour_coral) +
+  annotate("text", 
+           label = "\u03B2", 
+           x = -0.075, 
+           y = 0.85, 
+           size = 10/.pt, 
+           colour = "grey40") +
+  scale_fill_manual(values = c("#186AA5", "#C75E6B", "#186AA5")) +
+  scale_y_continuous(breaks = NULL) +
+  scale_x_continuous(breaks = 0) +
+  labs(y = NULL, 
+       x = NULL) +
+  theme(plot.background = elementalist::element_rect_round(radius = unit(0.85, "cm"), 
+                                                           color = "#FFE1E0"), 
+        axis.ticks = element_blank(), 
+        legend.position = "none") 
+
+
+
+
+# patch together and save -------------------------------------------------
+
+
+# patch together
+plot_final <- plot_paleotemp +
+  inset_element(plot_paleotemp_beta, 
+                left = 0.75, 
+                bottom = 0.6, 
+                right = 0.9,
+                top = 0.8) 
+
+
+
 # save plot
-ggsave(plot_paleotemp, filename = here("figures",
-                                       "effect_paleotemperature.png"), 
+ggsave(plot_final, filename = here("figures",
+                                   "effect_paleotemperature.png"), 
        width = image_width, height = image_height,
        units = image_units, 
        bg = "white", device = ragg::agg_png)
