b1cce94df73fbb86e04ed3a1259a626c23a2bce8
Ischi94 <gmathes94@gmx.de>
2023-04-05 22:23:53 +0200

    compare future predictions for iucn

--- R/2_analyze/future_predictions.R
+++ R/2_analyze/future_predictions.R
@@ -77,20 +77,76 @@ mod_fossil <- read_rds(here("data",
 # average predictions -----------------------------------------------------
 
 
+# for the future predictions
+# set up number of draws 
+nr_draws <- 1000
+
+# average prediction by model stacking
+dat_pred_iucn <- pp_average(mod1, mod2,
+                            mod3, mod4,
+                            newdata = dat_ipcc %>%
+                              filter(year >= 2023),
+                            seed = 1708,
+                            summary = FALSE,
+                            method = "posterior_epred",
+                            ndraws = nr_draws) %>%
+  as_tibble() %>% 
+  mutate(nr_draw = rownames(.)) %>% 
+  pivot_longer(cols = contains("V")) %>% 
+  add_column(year = rep(dat_ipcc %>% 
+                          filter(year >= 2023) %>% 
+                          pull(year), nr_draws)) %>% 
+  # bin to 10 year
+  mutate(year_bin = cut(year, breaks = seq(2020, 2100, by = 10),
+                        include.lowest = TRUE,
+                        labels = FALSE)) %>% 
+  group_by(year_bin) %>%
+  mean_qi(value) %>% 
+  add_column(year = seq(2020, 2090, by = 10)) %>% 
+  select(year, value, .lower, .upper)
+
+
+# save predictions
+dat_pred %>% 
+  write_rds(here("data", 
+                 "predictions", 
+                 "pred_temperature.rds"), 
+            compress = "gz")
+
 dat_ipcc %>% 
   filter(year >= 2023) %>% 
   mutate(temp_gat_binned = temp_gat_binned + 14) %>% 
   add_epred_draws(., mod_fossil[[1]]) %>% 
   group_by(year) %>% 
   median_qi(.epred) %>% 
+  mutate(.epred = (.epred - .$.epred[1]) / .$.epred[1], 
+         .lower = (.lower - .$.epred[1]) / .$.epred[1],
+         .upper = (.upper - .$.epred[1]) / .$.epred[1]) %>% 
   ggplot(aes(year, .epred)) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
   geom_line() +
-  geom_ribbon(aes(ymin = .lower, 
-                  ymax = .upper), 
+  geom_ribbon(aes(ymin = .lower,
+                  ymax = .upper),
               alpha = 0.2) +
-  scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1), 
-                     labels = c("0", "20", "40", "60", "80", "100")) +
-  coord_cartesian(ylim = c(0, 1)) +
+  # scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1), 
+  #                    labels = c("0", "20", "40", "60", "80", "100")) +
+  # coord_cartesian(ylim = c(0, 1)) +
+  coord_cartesian(ylim = c(-0.5, 0.5)) +
   labs(y = "Extinction Risk [%]", 
        x = "Year")
 
+
+
+dat_pred_iucn %>% 
+  mutate(value = (value - .$value[1]) / .$value[1], 
+         .lower = (.lower - .$value[1]) / .$value[1],
+         .upper = (.upper - .$value[1]) / .$value[1]) %>% 
+  ggplot(aes(year, value)) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  geom_ribbon(aes(ymin = .lower,
+                  ymax = .upper),
+              alpha = 0.2) +
+  geom_line() +
+  coord_cartesian(ylim = c(-1, 1)) +
+  labs(y = "Extinction Risk [%]", 
+       x = "Year")
