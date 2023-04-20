7b3de865e5fd5dca715fd55ad1abff0a5fdeefa4
Ischi94 <gmathes94@gmx.de>
2023-04-20 11:38:24 +0200

    remove 10myr and add future analysis

--- R/2_analyze/logit_comparison.R
+++ R/2_analyze/logit_comparison.R
@@ -14,7 +14,7 @@ nr_draws <- 100
 
 
 
-# fit deep-time models --------------------------------------------------------------
+# deep-time models --------------------------------------------------------------
 
 
 
@@ -78,7 +78,7 @@ dat_pred_deep %>%
 
 
 
-# fit genus models --------------------------------------------------------
+# genus models --------------------------------------------------------
 
 # read in cenozoic resolution data
 dat_merged <- read_rds(here("data",
@@ -140,69 +140,8 @@ dat_pred_genus %>%
                  "logit_genus.rds"))
 
 
-# fit 10myr models --------------------------------------------------------
 
-# read in cenozoic resolution data
-dat_merged <- read_rds(here("data",
-                            "processed_fossil_data_10myr.rds")) %>% 
-  mutate(bin = as.factor(bin))
-
-
-# start with deep ocean temperature
-# average over potential long-term trends
-mod1 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt1 + (1|bin)")
-mod2 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt2 + (1|bin)")
-mod3 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt3 + (1|bin)")
-mod4 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt4 + (1|bin)")
-
-# same for global average temperature
-mod5 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1 + (1|bin)")
-mod6 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2 + (1|bin)")
-mod7 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3 + (1|bin)")
-mod8 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4 + (1|bin)")
-
-
-# extract logit averaged over models
-
-# average prediction by model stacking
-# get model weights 
-mod_weights <- loo_model_weights(mod1, mod2,
-                                 mod3, mod4,
-                                 mod5, mod6,
-                                 mod7, mod8,
-                                 method = "pseudobma",
-                                 cores = parallelly::availableCores())
-
-
-# perform model averaging
-dat_pred_10myr <- distinct(dat_merged, bin) %>%
-  mutate(bin = as.integer(as.character(bin))) %>% 
-  pull(bin) %>% 
-  map_df(.f = ~ pp_average(mod1, mod2,
-                           mod3, mod4,
-                           mod5, mod6,
-                           mod7, mod8,
-                           newdata = dat_merged %>% 
-                             filter(bin == .x),
-                           seed = 1708,
-                           summary = FALSE,
-                           method = "posterior_linpred",
-                           ndraws = nr_draws, 
-                           weights = mod_weights) %>% 
-           as_tibble() %>% 
-           pivot_longer(cols = contains("V")) %>% 
-           mean_qi(value) %>% 
-           add_column(bin = .x) %>% 
-           select(value, .lower, .upper, bin))
-
-# save predictions
-dat_pred_10myr %>% 
-  write_rds(here("data", 
-                 "logits", 
-                 "logit_10myr.rds"))
-
-
-# fit cenozoic models --------------------------------------------------------------
+# cenozoic models --------------------------------------------------------------
 
 
 # read in cenozoic resolution data
@@ -253,7 +192,7 @@ dat_pred_ceno %>%
 
 
 
-# fit modern models --------------------------------------------------------------
+# modern models --------------------------------------------------------------
 
 
 # read iucn data
@@ -270,7 +209,7 @@ dat_iucn <- dat_iucn %>%
   mutate(ext_signal = if_else(iucn_status %in% c("LR/lc", "LC",
                                                  "LR / cd", "NT" ,
                                                  "LR / nt"),
-                              1, 0))
+                              0, 1))
 
 # read temperature data
 dat_ipcc <- tibble(filename = list.files(here("data",
@@ -328,6 +267,84 @@ dat_pred_modern %>%
 
 
 
+# future models -------------------------------------------------------
+
+# prepare temperature data 
+# load data
+dat_ipcc_fut <- tibble(filename = list.files(here("data",
+                                              "ipcc")) %>%
+                     str_remove("tas_global_") %>%
+                     str_remove(".csv")) %>% 
+  mutate(file_contents = map(list.files(here("data",
+                                             "ipcc"),
+                                        full.names = TRUE),  
+                             ~ read_csv(.x))) %>% 
+  unnest(file_contents) %>% # reformat
+  filter(filename %in% c("Historical", "SSP5_8_5")) %>% 
+  select(year = Year, 
+         temp_gat_binned = Mean) %>% 
+  arrange(desc(year)) %>% 
+  mutate(temp_gat_st = temp_gat_binned - lead(temp_gat_binned), 
+         temp_gat_lt1 = lead(temp_gat_st), 
+         temp_gat_lt2 = lead(temp_gat_st, n = 2), 
+         temp_gat_lt3 = lead(temp_gat_st, n = 3), 
+         temp_gat_lt4 = lead(temp_gat_st, n = 4))
+
+# prepare iucn-sim data 
+
+# load data
+dat_iucn_sim <- read_rds(here("data",
+                          "iucn_extinction_times.rds")) %>% 
+  # add reference time
+  mutate(ext_time = ext_time + 2023,   
+         year = map(ext_time, 
+                    ~ c(2023:.x))) %>% 
+  unnest(year) %>% 
+  # add extinction signal
+  mutate(ext_signal = if_else(ext_time == year, 1, 0)) 
+
+# merge
+
+# combine datasets
+dat_merged <- dat_iucn_sim %>% 
+  left_join(dat_ipcc_fut) %>% 
+  drop_na()
+
+
+# fit models
+
+mod1 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1")
+mod2 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2")
+mod3 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3")
+mod4 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4")
+
+
+# get model weights 
+mod_weights <- loo_model_weights(mod1, mod2,
+                                 mod3, mod4,
+                                 method = "pseudobma")
+
+
+# average posterior draws by model stacking
+dat_pred_future <- posterior_average(mod1, mod2,
+                                     mod3, mod4,
+                                     variable = c("b_temp_gat_binned"),
+                                     seed = 1708,
+                                     ndraws =  1e4,
+                                     missing = NA,
+                                     weights = mod_weights) %>% 
+  as_tibble() %>% 
+  mean_qi(b_temp_gat_binned) %>% 
+  select(value = b_temp_gat_binned, .lower, .upper)
+
+
+# save predictions
+dat_pred_future %>% 
+  write_rds(here("data", 
+                 "logits", 
+                 "logit_future.rds"))
+
+
 
 # visualise ---------------------------------------------------------------
 
@@ -356,14 +373,6 @@ dat_pred_full <- dat_pred_deep %>%
               left_join(dat_stages) %>%
               add_column(data_set = "genus") %>% 
               select(-bin)) %>% 
-  # add 10 myr resolution data
-  bind_rows(dat_pred_10myr %>% 
-              add_column(data_set = "10myr") %>% 
-              arrange(bin) %>% 
-              add_column(myr = seq(5, 135, by = 10)) %>% 
-              mutate(xmin = myr - 5, 
-                     xmax = myr + 5) %>% 
-              select(-bin)) %>% 
   # add cenozoic resolution data
   bind_rows(dat_pred_ceno %>% 
               rename(myr = bin) %>% 
@@ -375,7 +384,13 @@ dat_pred_full <- dat_pred_deep %>%
               add_column(data_set = "modern", 
                          myr = 0, 
                          xmin = 0, 
-                         xmax = 0)) 
+                         xmax = 0)) %>% 
+  # add future data
+  bind_rows(dat_pred_future %>% 
+              add_column(data_set = "future", 
+                         myr = 0, 
+                         xmin = -0.00002, 
+                         xmax = 0.00002)) 
 
 
 # save predictions
@@ -385,7 +400,8 @@ dat_pred_full %>%
                  "logit_full.rds"))
 
 # create plot
-plot_full <- dat_pred_full %>% 
+# plot_full <- 
+dat_pred_full %>% 
   ggplot(aes(myr, value, group = data_set)) +
   geom_hline(yintercept = 0, 
              colour = "grey20") +
