4a179d7f70706ad7e6987bddff3129dc582fc513
Ischi94 <gmathes94@gmx.de>
2023-05-25 16:25:01 +0200

    export temp plot

--- R/2_analyze/visualise_temp_trend.R
+++ R/2_analyze/visualise_temp_trend.R
@@ -11,5 +11,61 @@ source(here("R", "config_file.R"))
 # load data  ------------------------------------------------------------
 
 # get predictions from different scales/ data sets
-dat_merged <- read_rds(here("data",
-                            "processed_merged_data.rds"))
+dat_pred <- paste0("pred_temperature_", 
+       c("ceno", 
+         "genus", 
+         "stage"), 
+         ".rds") %>% 
+  map_df(~ read_rds(here("data",
+                         "predictions",
+                         .x)) %>% 
+           group_by(temperature) %>% 
+           slice_sample(n = 100) %>% 
+           ungroup() %>% 
+           add_column(dataset = .x) %>% 
+           mutate(dataset = str_remove_all(dataset, 
+                                           "pred_temperature_"), 
+                  dataset = str_remove_all(dataset, 
+                                           ".rds")))
+
+
+# visualise ---------------------------------------------------------------
+
+plot_temp <- dat_pred %>% 
+  mutate(dataset = case_when(
+    dataset == "ceno" ~ "1myr", 
+    dataset == "genus" ~ "Genus", 
+    dataset == "stage" ~ "Stages"
+  ), 
+  dataset = factor(dataset, 
+                   levels = c("Stages", 
+                              "Genus", 
+                              "1myr"))) %>% 
+  ggplot(aes(temperature,
+             value,
+             group = interaction(nr_draw, dataset),
+             colour = dataset)) +
+  geom_line(alpha = 0.2) +
+  scale_colour_manual(
+    values = c("#4C634C",
+               colour_coral, 
+               colour_purple),
+    name = NULL
+  ) +
+  guides(colour = guide_legend(override.aes = list(alpha = 0.9, 
+                                                   linewidth = 0.6))) +
+  scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1), 
+                     labels = c("0", "20", "40", "60", "80", "100")) +
+  coord_cartesian(ylim = c(0, 1), 
+                  xlim = c(0, 25)) +
+  labs(y = "Extinction Risk [%]", 
+       x = "Global Temperature [°C]") +
+  theme(legend.position = c(0.75, 0.75))
+
+
+# save plot
+ggsave(plot_temp, filename = here("figures",
+                                  "effect_temperature.png"),
+       width = image_width, height = image_height,
+       units = image_units,
+       bg = "white", device = ragg::agg_png)
