0e168fd0f71607b16331379b90e905d0dead1229
Ischi94 <gmathes94@gmx.de>
2023-06-29 16:58:39 +0200

    add tag levels

--- R/3_test/data_figures.R
+++ R/3_test/data_figures.R
@@ -28,12 +28,12 @@ plot_stages <- dat_stages %>%
     sr_mean_std,
     sea_level
   ) %>% 
-  rename("Shelf area [%]" = cont_area, 
-         "d13C [std]" = d13C_std, 
-         "Sea level [m]" = sea_level, 
-         "87Sr/ 86Sr [std]" = sr_mean_std, 
-         "Temperature deep [°C]" = temp_deep_binned, 
-         "Temperature gat [°C]" = temp_gat_binned) %>% 
+  rename("f) Shelf area [%]" = cont_area, 
+         "d) d13C [std]" = d13C_std, 
+         "c) Sea level [m]" = sea_level, 
+         "e) 87Sr/ 86Sr [std]" = sr_mean_std, 
+         "a) Temperature deep [°C]" = temp_deep_binned, 
+         "b) Temperature gat [°C]" = temp_gat_binned) %>% 
   pivot_longer(-bin) %>% 
   ggplot(aes(bin, value)) +
   geom_line(colour = "grey20") +
@@ -41,7 +41,9 @@ plot_stages <- dat_stages %>%
                      labels = c(150, 100, 50, 0), 
                      name = "Age [myr]") +
   facet_wrap(~ name, 
-             scales = "free") 
+             scales = "free") +
+  labs(y = NULL) +
+  theme(strip.text = element_text(hjust = 0))
 
 
 # save plot
@@ -69,17 +71,20 @@ plot_1myr <- dat_1myr %>%
     diatom_rich_std,
     sea_level
   ) %>% 
-  rename("Shelf area [%]" = cont_area, 
-         "d13C [std]" = d13C_std, 
-         "Sea level [m]" = sea_level,
-         "Diatom richness [std]" = diatom_rich_std, 
-         "Global temperature [°C]" = temp_binned) %>% 
+  rename("e) Shelf area [%]" = cont_area, 
+         "c) d13C [std]" = d13C_std, 
+         "b) Sea level [m]" = sea_level,
+         "d) Diatom richness [std]" = diatom_rich_std, 
+         "a) Global temperature [°C]" = temp_binned) %>% 
   pivot_longer(-bin) %>% 
   ggplot(aes(bin, value)) +
   geom_line(colour = "grey20") +
   scale_x_reverse(name = "Age [myr]") +
+  labs(y = NULL) +
   facet_wrap(~ name, 
-             scales = "free") 
+             scales = "free") +
+  theme(strip.text = element_text(hjust = 0))
+
 
 
 # save plot
@@ -103,10 +108,10 @@ plot_sampling <- dat_stages %>%
               select(bin = stg, 
                      age = mid)) %>% 
   select(-bin) %>% 
-  rename("Marine rock units [std]" = n_units_std, 
-         "Neoselachian collections [std]" = shark_collections_std, 
-         "PBDB collections [std]" = pbdb_collections_std,
-         "Marine outcrop area [std]" = outcrop_area_std) %>% 
+  rename("c) Marine rock units [std]" = n_units_std, 
+         "b) Neoselachian collections [std]" = shark_collections_std, 
+         "a) PBDB collections [std]" = pbdb_collections_std,
+         "d) Marine outcrop area [std]" = outcrop_area_std) %>% 
   pivot_longer(-age) %>%
   add_column(temp_scale = "Stages") %>% 
   bind_rows(dat_1myr %>% 
@@ -127,8 +132,11 @@ plot_sampling <- dat_stages %>%
                                 "grey40"), 
                      name = NULL) +
   scale_x_reverse(name = "Age [myr]") +
+  labs(y = NULL) +
   facet_wrap(~ name, 
-             scales = "free") 
+             scales = "free") +
+  theme(strip.text = element_text(hjust = 0))
+
 
 
 # save plot
