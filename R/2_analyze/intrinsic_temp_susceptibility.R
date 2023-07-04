da34e98d725a6771021c1169e1870dccbb26358f
Ischi94 <gmathes94@gmx.de>
2023-07-04 09:47:01 +0200

    finalise plot

--- R/2_analyze/intrinsic_temp_susceptibility.R
+++ R/2_analyze/intrinsic_temp_susceptibility.R
@@ -115,26 +115,26 @@ dat_combined %>%
 
 # visualise ---------------------------------------------------------------
 
-dat_combined %>% 
+plot_dep <- dat_combined %>%
   ggplot(aes(status, median_risk)) +
   geom_point(aes(fill = scale),
-             position = position_jitter(width = 0.05,
+             position = position_jitter(width = 0.1,
                                         seed = 123),
-              alpha = 0.7, 
+              alpha = 0.6, 
               shape = 21, 
               colour = "white",
-              size = 3.5) +
+              size = 2.3) +
   stat_halfeye(alpha = 0.5, 
                position = position_nudge(x = 0.1),
                limits = c(1, NA),
-               fill = "grey80",
-               slab_linewidth = 0.6,
+               fill = "white",
                point_interval = "mean_qi",
                shape = 21,
                point_alpha = 1,
                point_size = 4,
                point_fill = "white",
-               interval_alpha = 0) +
+               interval_alpha = 0.3, 
+               .width = 0.55) +
   geom_smooth(aes(as.numeric(status)),
               position = position_nudge(x = -1.9),
               method = "lm",
@@ -149,33 +149,67 @@ dat_combined %>%
               linewidth = 1) +
   annotate("text",
            y = 7, 
-           x = 0.6, 
-           colour = "grey30", 
-           size = 11/.pt, 
+           x = 5.8, 
+           colour = "grey40", 
+           size = 10/.pt, 
            label = "High susceptibility") +
   annotate("text",
            y = 58, 
-           x = 0.6, 
-           colour = "grey30", 
-           size = 11/.pt, 
+           x = 5.8, 
+           colour = "grey40", 
+           size = 10/.pt, 
            label = "Low susceptibility") +
   annotate("curve",
            y = 17, 
-           yend = 50, 
-           x = 0.6, 
-           xend = 0.6,
-           colour = "grey30", 
+           yend = 47, 
+           x = 5.8, 
+           xend = 5.8,
+           colour = "grey50", 
            curvature = 0,
            arrow = arrow(length = unit(.2,"cm"), 
                          ends = "both")) +
-  scale_fill_manual(values = c("#4C634C",
-                               colour_coral,
-                               colour_purple)) +
+  annotate("text",
+           y = 72, 
+           x = 1.2, 
+           colour = "grey40", 
+           size = 10/.pt, 
+           label = "Low risk") +
+  annotate("text",
+           y = 72, 
+           x = 5, 
+           colour = "grey40", 
+           size = 10/.pt, 
+           label = "High risk") +
+  annotate("curve",
+           y = 72, 
+           yend = 72, 
+           x = 1.5, 
+           xend = 4.7,
+           colour = "grey50", 
+           curvature = 0,
+           arrow = arrow(length = unit(.2,"cm"), 
+                         ends = "both")) +
+  scale_fill_manual(values = c("#4C634C", 
+                               colour_coral, 
+                               colour_purple), 
+                    labels = c("Stages - Species", 
+                               "Stages - Genus", 
+                               "1myr - Species"), 
+                    name = NULL) +
+  guides(fill = guide_legend(override.aes = list(alpha = 0.9))) +
   scale_y_continuous(breaks = c(1, 20, 40, 60)) +
   labs(x = "IUCN red list status", 
-       y = "Temperature susceptibility [ranked]") +
-  theme(legend.position = "none") + 
+       y = "Fossil temperature dependancy [ranked]") +
+  theme(legend.position = "bottom") +
   coord_flip()
+
+
+# save plot
+ggsave(plot_dep, filename = here("figures",
+                                 "fossil_temp_dependancy.png"), 
+       width = image_width, height = image_height,
+       units = image_units, 
+       bg = "white", device = ragg::agg_png)     
 
 
 
