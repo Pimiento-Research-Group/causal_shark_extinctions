25f43e7cec056241c098427061a38d680be6add5
Ischi94 <gmathes94@gmx.de>
2023-10-30 18:01:37 +0100

    calculate meta change for all scales

--- R/3_test/metabolism.R
+++ R/3_test/metabolism.R
@@ -30,15 +30,15 @@ dat_merged <- read_rds(here("data",
 # start with deep ocean temperature
 # average over potential long-term trends
 mod1 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt1 + (temp_deep_binned|family)")
-mod2 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt2 + (temp_deep_binned|order)")
-mod3 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt3 + (temp_deep_binned|order)")
-mod4 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt4 + (temp_deep_binned|order)")
+mod2 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt2 + (temp_deep_binned|family)")
+mod3 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt3 + (temp_deep_binned|family)")
+mod4 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt4 + (temp_deep_binned|family)")
 
 # same for global average temperature
-mod5 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1 + (temp_gat_binned|order)")
-mod6 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2 + (temp_gat_binned|order)")
-mod7 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3 + (temp_gat_binned|order)")
-mod8 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4 + (temp_gat_binned|order)")
+mod5 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1 + (temp_gat_binned|family)")
+mod6 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2 + (temp_gat_binned|family)")
+mod7 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3 + (temp_gat_binned|family)")
+mod8 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4 + (temp_gat_binned|family)")
 
 
 # extract logit averaged over models
@@ -114,21 +114,26 @@ dat_pred_deep %>%
 
 # read in genus resolution data
 dat_merged <- read_rds(here("data",
-                            "processed_fossil_data_genus.rds")) 
+                            "processed_fossil_data_genus.rds")) %>% 
+  # remove families with less than 3 occurrences
+  group_by(family) %>% 
+  mutate(n = n()) %>% 
+  ungroup() %>% 
+  filter(n >= 3)
 
 
 # start with deep ocean temperature
 # average over potential long-term trends
-mod1 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt1 + (temp_deep_binned|order)")
-mod2 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt2 + (temp_deep_binned|order)")
-mod3 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt3 + (temp_deep_binned|order)")
-mod4 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt4 + (temp_deep_binned|order)")
+mod1 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt1 + (temp_deep_binned|family)")
+mod2 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt2 + (temp_deep_binned|family)")
+mod3 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt3 + (temp_deep_binned|family)")
+mod4 <- brm_logistic("ext_signal ~ temp_deep_binned + temp_deep_st:temp_deep_lt4 + (temp_deep_binned|family)")
 
 # same for global average temperature
-mod5 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1 + (temp_gat_binned|order)")
-mod6 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2 + (temp_gat_binned|order)")
-mod7 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3 + (temp_gat_binned|order)")
-mod8 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4 + (temp_gat_binned|order)")
+mod5 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt1 + (temp_gat_binned|family)")
+mod6 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt2 + (temp_gat_binned|family)")
+mod7 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt3 + (temp_gat_binned|family)")
+mod8 <- brm_logistic("ext_signal ~ temp_gat_binned + temp_gat_st:temp_gat_lt4 + (temp_gat_binned|family)")
 
 
 # extract logit averaged over models
@@ -144,15 +149,15 @@ mod_weights <- loo_model_weights(mod1, mod2,
 
 
 # perform model averaging
-dat_pred_genus <- distinct(dat_merged, order) %>%
-  drop_na(order) %>% 
-  pull(order) %>% 
+dat_pred_genus <- distinct(dat_merged, family) %>%
+  drop_na(family) %>% 
+  pull(family) %>% 
   map_df(.f = ~ pp_average(mod1, mod2,
                            mod3, mod4,
                            mod5, mod6,
                            mod7, mod8,
                            newdata = filter(dat_merged, 
-                                            order == .x),
+                                            family == .x),
                            seed = 1708,
                            summary = FALSE,
                            method = "posterior_linpred",
@@ -160,15 +165,44 @@ dat_pred_genus <- distinct(dat_merged, order) %>%
                            weights = mod_weights) %>% 
            as_tibble() %>% 
            pivot_longer(cols = contains("V")) %>% 
-           mean_qi(value) %>% 
-           add_column(order = .x) %>% 
-           select(value, .lower, .upper, order))
+           add_column(family = .x) %>% 
+           select(value, family))
+
+# calculate percentage increase of selected families compared to endotherms
+dat_pred_genus <- dat_pred_genus %>% 
+  group_by(metab = if_else(!family %in% c("Lamnidae", "Otodontidae", "Alopiidae"),
+                           "meso", family)) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  mutate(value_sample = map(data, 
+                            ~ slice_sample(.x, n =  10000, replace = TRUE) %>% 
+                              select(value))) %>% 
+  select(-data) %>% 
+  pivot_wider(names_from = metab, values_from = value_sample) %>% 
+  mutate(oto = map2(Otodontidae, meso, 
+                    ~ (.x - .y)/.y),
+         oto = map(oto, median_qi),
+         lamni = map2(Lamnidae, meso, 
+                      ~ (.x - .y)/.y), 
+         lamni = map(lamni, median_qi), 
+         alo = map2(Alopiidae, meso, 
+                    ~ (.x - .y)/.y), 
+         alo = map(alo, median_qi))  %>% 
+  select(oto, lamni, alo) %>% 
+  unnest(cols = c(oto, lamni, alo), 
+         names_sep = "_") %>% 
+  select(where(is.double)) %>% 
+  pivot_longer(cols = everything(),
+               names_to = c("family", "pointval"),
+               names_sep = "_") %>% 
+  pivot_wider(names_from = pointval, 
+              values_from = value)
 
 # save predictions
 dat_pred_genus %>% 
   write_rds(here("data", 
-                 "logits", 
-                 "logit_order_genus.rds"))
+                 "metabolism", 
+                 "metab_family_genus.rds"))
 
 
 # cenozoic models --------------------------------------------------------------
@@ -176,14 +210,19 @@ dat_pred_genus %>%
 
 # read in cenozoic resolution data
 dat_merged <- read_rds(here("data",
-                            "processed_fossil_data_cenozoic.rds")) 
+                            "processed_fossil_data_cenozoic.rds")) %>% 
+  # remove families with less than 3 occurrences
+  group_by(family) %>% 
+  mutate(n = n()) %>% 
+  ungroup() %>% 
+  filter(n >= 3)
 
 
 # average over potential long-term trends
 mod1 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt1 + (temp_binned|family)")
-mod2 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt2 + (temp_binned|order)")
-mod3 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt3 + (temp_binned|order)")
-mod4 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt4 + (temp_binned|order)")
+mod2 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt2 + (temp_binned|family)")
+mod3 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt3 + (temp_binned|family)")
+mod4 <- brm_logistic("ext_signal ~ temp_binned + temp_st:temp_lt4 + (temp_binned|family)")
 
 
 # extract logit averaged over models
@@ -194,13 +233,13 @@ mod_weights <- loo_model_weights(mod1, mod2,
 
 
 # perform model averaging
-dat_pred_ceno <- distinct(dat_merged, order) %>%
-  drop_na(order) %>% 
-  pull(order) %>% 
+dat_pred_ceno <- distinct(dat_merged, family) %>%
+  drop_na(family) %>% 
+  pull(family) %>% 
   map_df(.f = ~ pp_average(mod1, mod2,
                            mod3, mod4,
                            newdata = filter(dat_merged, 
-                                            order == .x),
+                                            family == .x),
                            seed = 1708,
                            summary = FALSE,
                            method = "posterior_linpred",
@@ -208,20 +247,48 @@ dat_pred_ceno <- distinct(dat_merged, order) %>%
                            weights = mod_weights) %>% 
            as_tibble() %>% 
            pivot_longer(cols = contains("V")) %>% 
-           mean_qi(value) %>% 
-           add_column(order = .x) %>% 
-           select(value, .lower, .upper, order))
+           add_column(family = .x) %>% 
+           select(value, family))
 
+# calculate percentage increase of selected families compared to endotherms
+dat_pred_ceno <- dat_pred_ceno %>% 
+  group_by(metab = if_else(!family %in% c("Lamnidae", "Otodontidae", "Alopiidae"),
+                           "meso", family)) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  mutate(value_sample = map(data, 
+                            ~ slice_sample(.x, n =  10000, replace = TRUE) %>% 
+                              select(value))) %>% 
+  select(-data) %>% 
+  pivot_wider(names_from = metab, values_from = value_sample) %>% 
+  mutate(oto = map2(Otodontidae, meso, 
+                    ~ (.x - .y)/.y),
+         oto = map(oto, median_qi),
+         lamni = map2(Lamnidae, meso, 
+                      ~ (.x - .y)/.y), 
+         lamni = map(lamni, median_qi), 
+         alo = map2(Alopiidae, meso, 
+                    ~ (.x - .y)/.y), 
+         alo = map(alo, median_qi))  %>% 
+  select(oto, lamni, alo) %>% 
+  unnest(cols = c(oto, lamni, alo), 
+         names_sep = "_") %>% 
+  select(where(is.double)) %>% 
+  pivot_longer(cols = everything(),
+               names_to = c("family", "pointval"),
+               names_sep = "_") %>% 
+  pivot_wider(names_from = pointval, 
+              values_from = value)
 
 # save predictions
 dat_pred_ceno %>% 
   write_rds(here("data", 
-                 "logits", 
-                 "logit_order_ceno.rds"))
+                 "metabolism", 
+                 "metab_family_ceno.rds"))
 
 
 # merge together
-dat_order <- dat_pred_genus %>% 
+dat_family <- dat_pred_genus %>% 
   add_column(scale = "genus") %>% 
   full_join(dat_pred_deep %>% 
               add_column(scale = "stages")) %>% 
@@ -229,9 +296,12 @@ dat_order <- dat_pred_genus %>%
               add_column(scale = "ceno")) 
 
 # save data
-dat_order %>% 
+dat_family %>% 
   write_rds(here(here("data", 
-                      "logits", 
-                      "logit_order.rds")))
+                      "metabolism", 
+                      "metab_family.rds")))
 
+dat_family %>% 
+  group_by(family) %>% 
+  median_qi(value)
 
