ce1ff28ea5dc4efb32c984ff36e38b67371f93c5
Ischi94 <gmathes94@gmx.de>
2023-11-19 18:03:13 +0100

    unsummarised order logits

--- R/2_analyze/logit_order.R
+++ R/2_analyze/logit_order.R
@@ -123,10 +123,10 @@ dat_pred_genus <- distinct(dat_merged, order) %>%
                            ndraws = nr_draws, 
                            weights = mod_weights) %>% 
            as_tibble() %>% 
-           pivot_longer(cols = contains("V")) %>% 
-           mean_qi(value) %>% 
+           pivot_longer(cols = contains("V"), 
+                        values_to = "logit_val") %>% 
            add_column(order = .x) %>% 
-           select(value, .lower, .upper, order))
+           select(-name))
 
 # save predictions
 dat_pred_genus %>% 
